7b20cd92dd2c13392b7a2fcba99a4da44dadfcd8
mykas <mykolass@wix.com>
2020-04-16 11:27:06 +0300

    Initial

--- src/client.tsx
+++ src/client.tsx
@@ -1,22 +1,12 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 import { I18nextProvider } from 'react-i18next';
-import { wixAxiosConfig } from '@wix/wix-axios-config';
 import i18n from './i18n';
 import App from './components/App';
-import { create as createFedopsLogger } from '@wix/fedops-logger';
 
 const baseURL = window.__BASEURL__;
 const locale = window.__LOCALE__;
 
-wixAxiosConfig(axios, { baseURL });
-
-const fedopsLogger = createFedopsLogger('small');
-
-// Move the following `appLoaded()` call to the point where your app has fully loaded.
-// See https://github.com/wix-private/fed-infra/blob/master/fedops/fedops-logger/README.md
-fedopsLogger.appLoaded();
 
 ReactDOM.render(
   <Suspense fallback={'...loading'}>
